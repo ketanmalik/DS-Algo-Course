13860b990072adf6206e6cd13ba1568ab9ea4f79
ketanmalik <ketanmalik@gmail.com>
2021-10-13 12:55:36 -0400

    Test Java

---
+++ Test/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 5;
+        while (x > 0) {
+            System.out.println(x);
+            x--;
+        }
+    }
+}
