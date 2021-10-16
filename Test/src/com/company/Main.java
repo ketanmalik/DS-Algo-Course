fe2a9effbec4e90762469c67ea4ba8259b388520
ketanmalik <ketanmalik@gmail.com>
2021-10-16 11:43:21 -0400

    Adding sample code

--- Test/src/com/company/Main.java
+++ Test/src/com/company/Main.java
@@ -1,13 +1,26 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int x = 5;
-        while (x > 0) {
-            System.out.println(x);
-            x--;
+        int[] a1 = new int[]{1,2,3,4,5};
+        int[] a2 = new int[]{6,1,8};
+        boolean hasCommon = false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(Integer i: a1) {
+            if(map.get(i) == null) map.put(i, 1);
         }
+
+        for(Integer i: a2) {
+            if(map.get(i) != null) {
+                hasCommon = true;
+                break;
+            }
+        }
+
+        System.out.println(hasCommon);
     }
 }
