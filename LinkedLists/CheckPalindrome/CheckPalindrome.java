73fb4000f73464c07460adb0b9e24ac9bd50fdaf
ketanmalik <ketanmalik@gmail.com>
2021-11-01 19:28:05 -0400

    Check palindrome in linked list

---
+++ LinkedLists/CheckPalindrome/CheckPalindrome.java
@@ -0,0 +1,57 @@
+package CheckPalindrome;
+
+import ListNode.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckPalindrome {
+
+    // O(n) time & O(1) space
+    public boolean isPalindromeEfficient(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        fast = head;
+        slow = reverse(slow);
+
+        while(slow != null) {
+            if(slow.val != fast.val) return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+
+    // Helper function to reverse the linked list
+    public ListNode reverse(ListNode head) {
+        if(head == null) return null;
+        ListNode newHead = new ListNode(head.val);
+        head = head.next;
+        while(head != null) {
+            ListNode newNode = new ListNode(head.val, newHead);
+            newHead = newNode;
+            head = head.next;
+        }
+        return newHead;
+    }
+
+    // O(n) time & O(n) space
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while(head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int left = 0, right = list.size() - 1;
+        while(left < right) {
+            if(list.get(left++) != list.get(right--)) return false;
+        }
+        return true;
+    }
+}
